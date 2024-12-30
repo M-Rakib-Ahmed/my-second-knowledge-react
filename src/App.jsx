d7a16dfa3467646b5db33a1113c647cd58234ad3
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-12-30 22:51:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-4xl bg-red-400 mt-10 p-10 rounded-full text-white text-center font-bold'>Hello new bangladesh</h1>
+     
+    </>
+  )
+}
+
+export default App
